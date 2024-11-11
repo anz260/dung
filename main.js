fd319d1d2b84baa90a54d446d2eb8a52f1711cba
anz260 <patryk.piela1@gmail.com>
2024-11-11 13:56:50 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -7,10 +7,34 @@
 		"Wyspiarz Cideum",
 		"Satyr górski",
 		"Satyr leśny",
+	];		
+	
+	const sub_prof = [
+  "Kombatant",
+  "Gladiator",
+  "Poszukiwacz Przygód",
+  "Awanturnik",
+  "Gajowy",
+  "Pirat",
+  "Traper",
+  "Myśliwy",
+  "Szarlatan",
+  "Kupiec",
+  "Konsyliarz",
+  "Wagabunda",
+  "Akolita",
+  "Botanik",
+  "Wędrowiec",
+  "Paser",
+  "Wyrocznia",
+  "Kramarz",
+  "Opój",
+  "Badacz"
 	];	
 window.onload = function reset()
 { document.getElementById("race")[0].selected = true; 
  document.getElementById("occupation")[0].selected = true; 
+ 	document.getElementById('heroname').disabled = true;
 }
 	
 function info_race()
@@ -98,4 +122,137 @@ function info_occupation()
 	
 	document.querySelector('h1').innerHTML=h1[profesja];
 	document.getElementById('text_info').innerHTML=text_info[profesja];
+	
+
+	for (i=0;i<4;i++)
+	document.getElementById('hero_spec_prof')[i].innerHTML=sub_prof[i+4*profesja];
+	
+	
+}
+
+function info_background()
+{
+	const backgroundh = document.getElementById("hero_background").value;
+	const h1 = [
+  "Pomocnik Myśliwego",
+  "Dziecko Karczmarza",
+  "Poganiacz Zwierząt",
+  "Czeladnik Kowala",
+  "Rybak z Przystani",
+  "Złota Rączka",
+  "Osoba od Brudnej Roboty",
+  "Cmentarny Zbieracz"
+	];	
+	const text_info = [
+  "Dorastałeś, pomagając myśliwemu – przygotowywałeś obozowiska, tropiłeś zwierzęta i przynosiłeś trofea z polowań. Dzięki temu znasz doskonale las, a instynkt podpowiada ci, gdzie szukać śladów i kryjówek zwierząt.  <br><br>Tropienie +5, Przetrwanie +5",
+  
+  "Dorastałeś, pomagając rodzinie w karczmie, wśród opowieści podróżników i plotek klientów. Szybko nauczyłeś się rozumieć, co naprawdę ludzie mają na myśli, a twój urok potrafi przekonać do siebie nawet ponurego wojaka.  <br><br>Empatia +5, Gadanina +5",
+  
+  "Pracowałeś w handlu bydłem i wołami. Spędzając dni w drodze ze zwierzętami, nauczyłeś się rozpoznawać ich choroby i odpowiednio się nimi opiekować. Twoje umiejętności pomogą przetrwać każdej wyprawie.  <br><br>Wiedza o zwierzętach +5, Opieka nad zwierzętami +5",
+  
+  "Od młodości pomagałeś kowalowi, który uczył cię podstaw rzemiosła. Codzienna praca przy kowadle dała ci siłę i zręczność, a doświadczenie w rzemiośle jest doceniane przez innych rzemieślników.  <br><br>Kowalstwo +5, Krzepa +5",
+  
+  "Całe życie spędziłeś nad wodą, zarzucając sieci i wypływając na połów. Dzięki latom praktyki potrafisz ocenić, kiedy warunki są odpowiednie do żeglugi, i doskonale radzisz sobie z linami i węzłami.  <br><br>Żeglarstwo +5, Liny i węzły +5",
+  
+  "Od zawsze lubiłeś naprawiać różne przedmioty – czy to popsute zamki, czy stare meble. Z czasem opanowałeś sztukę majsterkowania, potrafiąc przywrócić do życia nawet zniszczone rzeczy. <br><br> Mechanika +5, Otwieranie zamków +5",
+  
+  "Byłeś najemnikiem, który podejmował się zadań, których inni się bali – od ochrony szemranych osobistości po brudne porachunki. Masz wprawę w brutalnej walce i przetrwaniu tam, gdzie inni nie chcą się zapuszczać. <br><br> Broń kłuta +1, Krzepa +5",
+  
+  "Zajmowałeś się nocnym poszukiwaniem wartościowych rzeczy na zapomnianych grobach i opuszczonych cmentarzach. Dzięki temu wiesz, jak cicho się poruszać, dostrzegać szczegóły w ciemnościach i radzić sobie z ryzykiem. <br><br> Ukrywanie się +5, Spostrzegawczość +5"
+];
+
+	
+	
+	document.querySelector('h1').innerHTML=h1[backgroundh];
+	document.getElementById('text_info').innerHTML=text_info[backgroundh];	
+	
+}
+
+
+
+function info_spec_prof()
+{
+	const spec_profesja = document.getElementById("hero_spec_prof").value;
+	const profesja = document.getElementById("occupation").value;
+	var real_prof =parseInt(profesja*4)+parseInt(spec_profesja);
+	const text_info = [
+"<ul><li>Zdolności fizyczne X4</li><li>Zdolności społeczne X1</li><li>Zdolności naturalne X2</li><li>Zdolności uczone X1</li><li>Zdolności cechowe X1</li><li>Techniki walki X3</li></ul>",
+
+"<ul><li>Zdolności fizyczne X4</li><li>Zdolności społeczne X0</li><li>Zdolności naturalne X3</li><li>Zdolności uczone X0</li><li>Zdolności cechowe X2</li><li>Techniki walki X3</li></ul>",
+
+"<ul><li>Zdolności fizyczne X3</li><li>Zdolności społeczne X2</li><li>Zdolności naturalne X2</li><li>Zdolności uczone X1</li><li>Zdolności cechowe X2</li><li>Techniki walki X2</li></ul>",
+
+"<ul><li>Zdolności fizyczne X6</li><li>Zdolności społeczne X2</li><li>Zdolności naturalne X2</li><li>Zdolności uczone X0</li><li>Zdolności cechowe X2</li><li>Techniki walki X1</li></ul>",
+
+
+
+];
+
+	document.querySelector('h1').innerHTML=sub_prof[real_prof];
+	document.getElementById('text_info').innerHTML="Wartość Xn symbolizuje ile profesji możesz wybrać w zdolności"+text_info[real_prof];	
+	
+}
+
+
+
+function info_exp()
+{
+	const doswiadczenie = document.getElementById("experience").value;
+
+	const text_info = [
+"2K6+7",
+"K12+6",
+"K10+6",
+"K8+6",
+"K6+6",
+
+
+
+];
+
+	document.querySelector('h1').innerHTML=document.getElementById("experience")[doswiadczenie].textContent;
+	document.getElementById('text_info').innerHTML="Podczas tworzenia postaci, wyniki rzutów na atrybuty podstawowe są oparte o "+text_info[doswiadczenie]+'.';	
+	
+}
+
+
+/*/
+/*/
+/*/
+/*/
+
+function losowanie()
+{
+	
+	
+	imiona();
+
+}
+
+function imiona()
+{
+	
+	
+	const n1 = 
+	[
+	  "Aeri", "Alka", "Arka", "Aster", "Drak", 
+	  "Dyma", "Erythi", "Gala", "Kaian", "Kly", 
+	  "Lyko", "Nyk", "Phok", "Pyro", "Sarpi", 
+	  "Ther", "Theran", "Xan", "Zephi", "Zelor"
+	];
+	const n2 = [
+	  "doros", "nor", "lon", "iades", "mynos", 
+	  "rkos", "on", "kryn", "zos", "moros", 
+	  "rion", "onis", "aris", "thon", "des", 
+	  "diros", "toros", "theon", "rus", "medes"
+	];
+
+	
+	var r1=Math.floor(Math.random()*20);
+	var r2=Math.floor(Math.random()*20);
+	
+	document.getElementById('heroname').value=n1[r1]+n2[r2];
+	document.getElementById('heroname').disabled = false;
+	
+	
 }
